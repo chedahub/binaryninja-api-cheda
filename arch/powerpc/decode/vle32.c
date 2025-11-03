28d461609ef6cb806fe6b4460cbd4d03bca90c35
chedahub <2017000029@ushs.hs.kr>
2025-11-03 17:12:13 +0900

    update

--- arch/powerpc/decode/vle32.c
+++ arch/powerpc/decode/vle32.c
@@ -383,7 +383,6 @@ static void FillOperands32Vle(Instruction* instruction, uint32_t word32, uint64_
 		case PPC_ID_VLE_E_AND2IS:
 		case PPC_ID_VLE_E_OR2I:
 		case PPC_ID_VLE_E_OR2IS:
-		case PPC_ID_VLE_E_LIS:
 			uint32_t ui5_15 = word32 & 0x7ff;
 			uint32_t ui0_4 = (word32 >> 16) & 0x1f;
 			uint32_t ui = (ui0_4 << 11) | ui5_15;
@@ -395,6 +394,14 @@ static void FillOperands32Vle(Instruction* instruction, uint32_t word32, uint64_
 			PushUIMMValue(instruction, ui);
 			if ((instruction->id == PPC_ID_VLE_E_AND2I) || (instruction->id == PPC_ID_VLE_E_AND2IS))
 				instruction->flags.rc = true;
+			break;
+
+		case PPC_ID_VLE_E_LIS:
+			uint32_t ui5_15 = word32 & 0x7ff;
+			uint32_t ui0_4 = (word32 >> 16) & 0x1f;
+			uint32_t ui = (ui0_4 << 11) | ui5_15;
+			PushRD(instruction, word32);
+			PushUIMMValue(instruction, ui);
 			break;
 
 		// <op>[.] rA, rS, SH
