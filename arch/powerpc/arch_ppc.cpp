d69f44479c206268bd5bdf6fe2e2483fa3e00e16
chedahub <2017000029@ushs.hs.kr>
2025-11-06 23:07:09 +0900

    update auto liftOps

--- arch/powerpc/arch_ppc.cpp
+++ arch/powerpc/arch_ppc.cpp
@@ -829,16 +829,8 @@ class PowerpcArchitecture: public Architecture
 		}
 
 		auto liftOps = [&]() {
-			if ((op == LLIL_SUB) || (op == LLIL_FSUB))
-			{
-				left = il.GetExprForRegisterOrConstant(operands[0], size);
-				right = il.GetExprForRegisterOrConstant(operands[1], size);
-			}
-			else
-			{
-				left = il.GetExprForRegisterOrConstantOperation(op, size, operands, operandCount);
-				right = il.Const(size, 0);
-			}
+			left = il.GetExprForRegisterOrConstantOperation(op, size, operands, operandCount);
+			right = il.Const(size, 0);
 		};
 
 		switch (flag)
